7861114d21552e17b55880f2c775383d97f9c93d
Taras <hevko.tars@gmail.com>
2021-07-16 13:35:45 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.77f208bc1351331d37565e7b7ca0464e.js"
 );
 
 self.addEventListener('message', (event) => {
